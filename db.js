54cbc8a07cd99a0289013698d16ed75fa7991c53
Az1z_404 <u03062010@gmail.com>
2025-08-19 16:58:25 +0500

    part1

--- db.js
+++ db.js
@@ -4,11 +4,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     // IPv6/localhost muammolarini chetlash uchun 127.0.0.1
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // 5 soniya ichida serverni tanlash
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ MongoDB ulanish holati: Ulandi");
   } catch (error) {
     console.error("❌ MongoDB ulanishida xatolik:", error.message);
